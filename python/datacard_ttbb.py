d966060cbd7c503f70159108e3f54eb6272d8913
Darren Michael Puigh <darren.michael.puigh@cern.ch>
2013-05-27 19:59:43 +0000

    include 50% tt+cc rate uncertainty

--- python/datacard_ttbb.py
+++ python/datacard_ttbb.py
@@ -578,7 +578,8 @@ def create_datacard(ifile, ofile, disc, all_categories,
             "lumi": "1.044" if is_8_tev else "1.022",
             "CMS_ttH_eff_lep": "1.04" if is_8_tev else "1.018",
             "CMS_ttH_QCDscale_ttb": "1.5",
-            "CMS_ttH_QCDscale_ttbb": "1.5"}
+            "CMS_ttH_QCDscale_ttbb": "1.5",
+            "CMS_ttH_QCDscale_ttcc": "1.5"}
 
     # Retrieve list of samples (ordered) from systematics file
     samples = get_systematics(sysfile, samples=True)
